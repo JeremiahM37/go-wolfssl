57d08213475a15cbb8c6bd86d7824c7c7a80fb52
Jeremiah Mackey <jeremiahmackey91@gmail.com>
2026-03-20 15:56:22 +0000

    Bounds-check WolfSSL_read/write

--- ssl.go
+++ ssl.go
@@ -214,10 +214,18 @@ func WolfSSL_accept(ssl *C.struct_WOLFSSL) int {
 }
 
 func WolfSSL_read(ssl *C.struct_WOLFSSL, data []byte, sz uintptr) int {
+    if sz > uintptr(len(data)) {
+        sz = uintptr(len(data))
+    }
+    if sz == 0 { return 0 }
     return int(C.wolfSSL_read(ssl, unsafe.Pointer(&data[0]), C.int(sz)))
 }
 
 func WolfSSL_write(ssl *C.struct_WOLFSSL, data []byte, sz uintptr) int {
+    if sz > uintptr(len(data)) {
+        sz = uintptr(len(data))
+    }
+    if sz == 0 { return 0 }
     return int(C.wolfSSL_write(ssl, unsafe.Pointer(&data[0]), C.int(sz)))
 }
 
