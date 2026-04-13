52b1e6c68593ccdfb46b401fd78c571af53e154f
Lealem Amedie <lealem@wolfssl.com>
2026-04-13 15:13:39 -0600

    Fix arg checking bug in Wc_HmacFinal

--- hmac.go
+++ hmac.go
@@ -94,7 +94,7 @@ func Wc_HmacUpdate(hmac *C.struct_Hmac, in []byte, inSz int) int {
 }
 
 func Wc_HmacFinal(hmac *C.struct_Hmac, out []byte) int {
-    if len(out) < int(C.WC_MAX_DIGEST_SIZE) { return BAD_FUNC_ARG }
+    if len(out) == 0 { return BAD_FUNC_ARG } 
     return int(C.wc_HmacFinal(hmac, (*C.uchar)(unsafe.Pointer(&out[0]))))
 }
 
